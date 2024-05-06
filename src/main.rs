a7dc570c8db679e80327197268847e13c83306d0
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2024-05-06 14:54:23 +0400

    x

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::str::FromStr;
+
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn getip(req: HttpRequest) -> impl Responder {
+    let con_info = req.connection_info().clone();
+    let ip = con_info.realip_remote_addr().unwrap();
+    let formed_ip = String::from_str(ip).unwrap();
+    HttpResponse::Ok().body(formed_ip)
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(getip)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
